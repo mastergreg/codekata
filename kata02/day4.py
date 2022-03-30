758a256b3fefef5669a0faabb3234ad0877416c6
Greg Lyras <greglyras@gmail.com>
2022-03-30 09:14:47 +0100

    implemented from other source

--- kata02/day4.py
+++ kata02/day4.py
@@ -1,2 +1,12 @@
 def chop(search: int, array: list[int]):
-    pass
+    left = 0
+    right = len(array) - 1
+    while left <= right:
+        pivot = (left + right) // 2
+        if array[pivot] < search:
+            left = pivot + 1
+        elif array[pivot] > search:
+            right = pivot - 1
+        else:
+            return pivot
+    return -1
